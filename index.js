2c3e29e9279d80838b644d12ecea20fb4c53bde3
Rafael Takeguma <rafael08.0807@gmail.com>
2024-02-29 00:20:32 -0300

    add get endpoint

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
 const app = require('express')();
 const PORT = 8080;
 
+// setting up the server with this endpoint
+app.get('/persons', (request, response) => {
+    response.status(200).send({
+        person: 'Rafael',
+        age: 21 
+    })
+});
+
 // listening for connections, listening on a specific port (8080). Firing up the API on the server
+
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
